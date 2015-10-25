7fbcd02f420746a82361519f680b2f780a8370b9
FF2015 <faiz.f@aol.com>
2015-10-25 00:52:36 -0400

    Updated code in 'cacheSolve'

--- cachematrix.R
+++ cachematrix.R
@@ -47,28 +47,7 @@ cacheSolve <- function(x, ...) {
         # create matrix since it does not exist
         matrix <- x$get()
 
-        # make sure matrix is square and invertible
-        # if not, handle exception cleanly
-        tryCatch( {
-                # set and return inverse of matrix
-                cache <- solve(matrix, ...)
-        },
-        error = function(e) {
-                message("Error:")
-                message(e)
-
-                return(NA)
-        },
-        warning = function(e) {
-                message("Warning:")
-                message(e)
-
-                return(NA)
-        },
-        finally = {
-                # set inverted matrix in cache
-                x$setMatrix(cache)
-        } )
+      
 
         # display matrix in console
         return (cache)
